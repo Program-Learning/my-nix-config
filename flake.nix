a8ffe147f843279f584e4c9dd1e17929a216fb17
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2023-11-09 02:57:27 +0800

    Add mirror for cachix

--- flake.nix
+++ flake.nix
@@ -50,4 +50,20 @@
     };
 
   };
+  nixConfig = {
+    experimental-features = [ "nix-command" "flakes" ];
+    substituters = [
+      # replace official cache with a mirror located in China
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+        "https://cache.nixos.org/"
+    ];
+
+    # nix community's cache server
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 }
