8d3f472f71805e942d3e7c5ee7f73f35eb8f207d
zakoxako <102341238+DataEraserC@users.noreply.github.com>
2023-11-08 18:24:53 +0000

    add essential packages

--- nix-on-droid.nix
+++ nix-on-droid.nix
@@ -5,23 +5,29 @@
   environment.packages = with pkgs; [
     # User-facing stuff that you really really want to have
     vim # or some other editor, e.g. nano or neovim
+    git
+    kicad
+    logisim-evolution
+    gimp
+    neofetch
+    openssh
 
     # Some common stuff that people expect to have
-    #diffutils
-    #findutils
-    #utillinux
-    #tzdata
-    #hostname
-    #man
-    #gnugrep
-    #gnupg
-    #gnused
-    #gnutar
-    #bzip2
-    #gzip
-    #xz
-    #zip
-    #unzip
+    diffutils
+    findutils
+    utillinux
+    tzdata
+    hostname
+    man
+    gnugrep
+    gnupg
+    gnused
+    gnutar
+    bzip2
+    gzip
+    xz
+    zip
+    unzip
   ];
 
   # Backup etc files instead of failing to activate generation if a file already exists in /etc
