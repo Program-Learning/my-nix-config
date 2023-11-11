1f0665c539d54b7c27378e0901bb5ee320c6a1d9
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2023-11-11 19:49:59 +0800

    Add some essential tools

--- home/nix-on-droid/aarch64/mondrian_1/modules/development.nix
+++ home/nix-on-droid/aarch64/mondrian_1/modules/development.nix
@@ -1,34 +1,37 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  my_python = pkgs.python311.withPackages (ps:
+    with ps; [
+      ipykernel
+      jupyterlab
+      matplotlib
+      numpy
+      seaborn
+      networkx
+      beautifulsoup4
+      selenium
+      urllib3
+      pyclip
+      pybluez
+      pymysql
+      jieba
+      wordcloud
+      pandas-datareader
+      pip # use in venv "python -m venv .venv" "source .venv/bin/activate"
+
+      ipython
+      pandas
+      requests
+      pyquery
+      pyyaml
+    ]);
+in {
   home.packages = with pkgs; [
+    my_python
+
     rsync
     ranger
     proxychains
     tmux
-    (python311.withPackages (ps:
-      with ps; [
-        ipykernel
-        jupyterlab
-        matplotlib
-        numpy
-        seaborn
-        networkx
-        beautifulsoup4
-        selenium
-        urllib3
-        pyclip
-        pybluez
-        pymysql
-        jieba
-        wordcloud
-        pandas-datareader
-        pip # use in venv "python -m venv .venv" "source .venv/bin/activate"
-
-        ipython
-        pandas
-        requests
-        pyquery
-        pyyaml
-      ]))
 
     # User-facing stuff that you really really want to have
     vim # or some other editor, e.g. nano or neovim
@@ -56,6 +59,10 @@
     ncurses # command clear
     which
     htop
+    tree
+    netcat # nc
+    procps # command ps/pgrep and ...
+    lsof
   ];
   programs = {
     zsh = {
